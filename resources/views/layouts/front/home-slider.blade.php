dc96c25eaf9894eb5ab40bfbfd698adccf56c157
Aditya Pratama <orochimadit@gmail.com>
2019-04-16 17:50:34 +0700

    wanoja Baru

---
+++ resources/views/layouts/front/home-slider.blade.php
@@ -0,0 +1,14 @@
+<section id="hero" class="hero-section top-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <div class="hero-content">
+                    <h1 class="hero-title">{{config('app.name')}}  <br> Collection</h1>
+                    <p class="hero-text">Wanoja Toko Onlune</p>
+                    
+                </div>
+            </div>
+            <div class="col-md-4"></div>
+        </div>
+    </div>
+</section>
